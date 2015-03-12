4e3beb30ffd1d494e7f377e282cc1cc58350a4d5
mecmaster <oleksandr.dzyuba@gmail.com>
2015-03-12 14:28:24 +0200

    insta new

--- sid/js/app.js
+++ sid/js/app.js
@@ -5,14 +5,14 @@ $(function() {
         clientId: '2087c51c4d644da1bef8660fa8204a93',
         target: 'instafeed',
         get: 'user',
-        userId: 186040369,
-        accessToken: '186040369.467ede5.2c7261dc355e411fb972e9d516a181c5',
+        userId: 1575786723,
+        accessToken: '1575786723.467ede5.5689c21d86f94839b59ce0f1536d85d3',
         useHttp: true,
         links: true,
         limit: 8,
         sortBy: 'most-recent',
         resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.created_time}}</div></div></div></div>'
     });
     feed.run();
  
